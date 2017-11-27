620e1f9dbad73ec668b42694683c31614388dff1
krechanski <kirohammet@gmail.com>
2017-11-27 00:44:06 +0000

    Added JavaDoc comments to Displacement.java

--- src/tourguide/Displacement.java
+++ src/tourguide/Displacement.java
@@ -2,12 +2,23 @@ package tourguide;
 
 import java.util.logging.Logger;
 
+/** The class Displacement calculates the position of a given location
+ *  relative to another.
+ *  @author Paul Jackson
+ **/
+
 public class Displacement {
     private static Logger logger = Logger.getLogger("tourguide");
        
     public double east;
     public double north;
-    
+
+    /** This method initializes the variables east and north which are declared at the
+     * beginning of the class.
+     *
+     * @param e Stands for 'east'
+     * @param n Stands for 'north
+     * */
     public Displacement(double e, double n) {
         logger.finer("East: " + e + "  North: "  + n);
         
@@ -15,19 +26,27 @@ public class Displacement {
         north = n;
     }
 
-
+    /** This method returns the the distance between two points using the
+     *  distance formula.
+     *
+     * @return calculated distance using the Math.sqrt() function.
+     */
     public double distance() {
         logger.finer("Entering");
         
         return Math.sqrt(east * east + north * north);
     }
-    
-    // Bearings measured clockwise from north direction.
+
+
+    /** This method returns the bearings measured clockwise from north direction.
+     * Also atan2(y,x) computes angle from x-axis towards y-axis, returning a negative result
+     * when y is negative.
+     *
+     * @return The angle converted to degrees from radians.
+     */
+
     public double bearing() {
         logger.finer("Entering");
-              
-        // atan2(y,x) computes angle from x-axis towards y-axis, returning a negative result
-        // when y is negative.
         
         double inRadians = Math.atan2(east, north);
         
